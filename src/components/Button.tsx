9f12152922209eff9bb5ce51e074c190e9ed3c8f
mdsiaofficial <mdsiaofficial@gmail.com>
2024-07-31 16:37:58 +0600

    "Updated Button.tsx: added newProps interface, Garden interface, and modified Field component with new state, event handlers, and form elements."

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,16 +7,22 @@ type Props2 = {
   text: string
 }
 
-interface newProps{
+interface newProps {
   text: string | number;
   onclick?: () => void;
   age?: number;
 }
 
+interface Garden {
+  name: string;
+  trees: number;
+  flowers: number;
+}
+
 const Button1: React.FC<Props1> = (props) => {
   return (
     <button>
-      <div>Button - { props.text }</div>
+      <div>Button - {props.text}</div>
     </button>
   )
 }
@@ -24,22 +30,85 @@ const Button1: React.FC<Props1> = (props) => {
 const Button2: React.FC<Props2> = (props) => {
   return (
     <button>
-      <div>Button - { props.text }</div>
+      <div>Button - {props.text}</div>
     </button>
   )
 }
 
 const Field: React.FC<newProps> = (props) => {
-  const [val, setVal] = useState();
+  const [val, setVal] = useState<number>(0);
+  const [gard, setGard] = useState<Garden>({
+    name: "Ashiq",
+    trees: 99,
+    flowers: 45,
+  })
+
+  const inputName = document.getElementById("name") as HTMLInputElement;
+  const inputTrees = document.getElementById("trees") as HTMLInputElement;
+  const inputFlowers = document.getElementById("flowers") as HTMLInputElement;
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+
+    setGard({
+      name: inputName.value,
+      trees: parseInt(inputTrees.value),
+      flowers: parseInt(inputFlowers.value),
+    })
+  }
+
+  const handleSubmit2 = (e:React.FormEvent <HTMLFormElement>) => {
+    e.preventDefault();
+
+    setGard({
+      name: e.target.,
+      trees: Number(inputTrees.value),
+      flowers: Number(inputFlowers.value),
+    })
+  }
+
   return (
     <div>
       <h1>{props.text}</h1>
       {props.onclick && <button onClick={props.onclick}>Click</button>}
       <p>{val}</p>
-      {props.onclick && <button onClick={()=>setVal(val+1)}>Click</button>}
+      {props.onclick && <button onClick={() => setVal(val + 1)}>Click</button>}
       {props.age && <p>Age: {props.age}</p>}
+
+      <section>
+        Garden
+        <p> {gard.name} </p>
+        <p> {gard.trees} </p>
+        <p> {gard.flowers} </p>
+
+        <div className="">
+          <form action="">
+            <input type="text" id='name' />
+            <input type="number" id='trees' />
+            <input type="number" id='flowers' />
+            <button onClick={handleSubmit}>Submit</button>
+          </form>
+        </div>
+      </section>
+
+      {/* 2 */}
+      <section>
+        Garden
+        <p> {gard.name} </p>
+        <p> {gard.trees} </p>
+        <p> {gard.flowers} </p>
+
+        <div className="">
+          <form action="" onSubmit={handleSubmit2}>
+            <input type="text" id='name' />
+            <input type="number" id='trees' />
+            <input type="number" id='flowers' />
+            <button type='submit'>Submit</button>
+          </form>
+        </div>
+      </section>
     </div>
   )
 }
 
-export {Button1, Button2, Field}
+export { Button1, Button2, Field }
